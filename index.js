8ac03fc7acbee2f17179e1f1bd111a46862670e9
Gustavo Eumana <gustavo.eumana@student.uncommonhs.org>
2017-02-08 22:25:42 +0000

    I changed stuff

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 
 $(document).ready( function(){
+    
+    
+    
     $('#change-color').click(function(){
-        $('body').css("background-color","red");
-    })
-})
-
+        $('body').css("background-color","blue");
+        var colors = ["#CCCCCC","#333333","#990099"];                
+        var rand = Math.floor(Math.random()*colors.length);           
+        $('body').css("background-color",colors[rand]);
+    });
+   $("#submit-name").click(function() {
+        var name = $("#name").val();
+        $("#greeting").append(name);
+   });
+   $("#pic1").click(function() {
+       $("#gallery-main").attr("src", "https://upload.wikimedia.org/wikipedia/commons/f/f9/Loxodonta_africana_-_old_bull_(Ngorongoro,_2009).jpg");
+   });
+     $("#pic2").click(function() {
+       $("#gallery-main").attr("src", "https://upload.wikimedia.org/wikipedia/commons/6/63/African_elephant_warning_raised_trunk.jpg");
+   });
+     $("#pic3").click(function() {
+       $("#gallery-main").attr("src", "https://iso.500px.com/wp-content/uploads/2014/08/2048-5.jpg");
+   });
+});
